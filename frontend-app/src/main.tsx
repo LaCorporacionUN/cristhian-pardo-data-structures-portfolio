b1753f5fe42a64ce5a787b631f32096d41418176
Cristhian <crijpardomer@unal.edu.co>
2025-07-15 06:55:48 -0500

    fr4

--- frontend-app/src/main.tsx
+++ frontend-app/src/main.tsx
@@ -4,15 +4,15 @@ import App from './App'
 import './i18n'
 import theme from './theme'
 import { ChakraProvider, ColorModeScript } from '@chakra-ui/react'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter  } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <ChakraProvider theme={theme}>
       <ColorModeScript initialColorMode={theme.config.initialColorMode} />
-      <BrowserRouter>
+      <HashRouter>
         <App />
-      </BrowserRouter>
+      </HashRouter>
     </ChakraProvider>
   </React.StrictMode>
 )
