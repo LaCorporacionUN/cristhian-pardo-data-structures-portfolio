b975326a910752d0f812df78307490bdda151dd4
Cristhian <crijpardomer@unal.edu.co>
2025-07-17 05:00:04 -0500

    adding excercises from 3 to 13

--- frontend-app/src/markdown/12. Disjoint Set/Ejercicio5Page.tsx
+++ frontend-app/src/markdown/12. Disjoint Set/Ejercicio5Page.tsx
@@ -2,7 +2,7 @@
 import React from 'react'
 import ReactMarkdown from 'react-markdown'
 import { Box, VStack } from '@chakra-ui/react'
-import content from './ejercicio12_5.md?raw'
+import content from './ejercicio5.md?raw'
 
 export default function Ejercicio12_5Page() {
   return (
