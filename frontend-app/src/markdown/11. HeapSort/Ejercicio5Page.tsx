559a04667121eb085071734e16997962155e947f
Cristhian <crijpardomer@unal.edu.co>
2025-07-17 05:00:25 -0500

    adding excercises from 3 to 13

--- frontend-app/src/markdown/11. HeapSort/Ejercicio5Page.tsx
+++ frontend-app/src/markdown/11. HeapSort/Ejercicio5Page.tsx
@@ -2,7 +2,7 @@
 import React from 'react';
 import ReactMarkdown from 'react-markdown';
 import { Box, VStack } from '@chakra-ui/react';
-import content from './ejercicio11_5.md?raw';
+import content from './ejercicio5.md?raw';
 
 export default function Ejercicio11_5Page() {
   return (
